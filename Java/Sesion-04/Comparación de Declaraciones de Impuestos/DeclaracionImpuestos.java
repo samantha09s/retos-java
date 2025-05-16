3dedaf0cf61ea874fa86c14934d99e3d506bf7f8
Samantha Munguía <140031528+samantha09s@users.noreply.github.com>
2025-05-15 22:51:40 -0600

    Delete

--- Java/Sesion-04/Comparación de Declaraciones de Impuestos/DeclaracionImpuestos.java
+++
@@ -1,7 +0,0 @@
-// Record que representa una declaración de impuestos, usando un tipo inmutable
-// para asegurar la integridad de los datos fiscales.
-public record DeclaracionImpuestos(String rfcContribuyente, double montoDeclarado) {
-
-    // El constructor y los campos son implícitamente públicos y finales
-    // por lo tanto, no necesitamos definir getters manualmente.
-}
